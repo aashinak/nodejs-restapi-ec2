3ee14f997f8084cce94759d9e575ba6e9200e3b8
aashinak <aashin9605@gmail.com>
2024-09-01 12:41:34 +0530

    update get req

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT;
 
-app.get("/ping", (req, res) => {
-  res.json({ message: "pong!!!" });
+app.get("/api/ping", (req, res) => {
+  res.status(200).json({ message: "pong!!!", success: true });
 });
 app.listen(port, () => {
   console.log("Server firing at port", port);
